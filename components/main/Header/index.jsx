e4cfc77fe3a72169b5c84b2f145b889b6c0f550f
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-06-01 22:56:41 +0300

    add navbar

--- components/main/Header/index.jsx
+++ components/main/Header/index.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 import styles from './Header.module.scss';
 
 const Header = () => {
+    const navbarItems = [
+        { id: '#about', title: 'About' },
+        { id: '#chapters', title: 'Chapters' },
+        { id: '#reviews', title: 'Reviews' },
+        { id: '#author', title: 'Author' },
+        { id: '#contact', title: 'Contact' },
+    ];
+
     return (
         <div className={styles.headerWrapper}>
             <div className={'container'}>
                 <div className={styles.header}>
                     <div className={styles.logo}>logo</div>
-                    <div className={styles.navbar}>nav</div>
+                    <div className={styles.navbar}>
+                        <ul className={styles.list}>
+                            {navbarItems.map((navItem) => (
+                                <li
+                                    key={navItem.id}
+                                    className={styles.listItem}
+                                >
+                                    {navItem.title}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                     <div className={styles.socialIcons}>socialIcons</div>
                 </div>
             </div>
