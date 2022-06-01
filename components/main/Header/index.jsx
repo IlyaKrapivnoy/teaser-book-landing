73420161a322cfabf91974d648895357b53afcd4
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-06-01 22:45:39 +0300

    separate header to parts

--- components/main/Header/index.jsx
+++ components/main/Header/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styles from './Header.module.scss';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className={styles.headerWrapper}>
+            <div className={'container'}>
+                <div className={styles.header}>
+                    <div className={styles.logo}>logo</div>
+                    <div className={styles.navbar}>nav</div>
+                    <div className={styles.socialIcons}>socialIcons</div>
+                </div>
+            </div>
         </div>
     );
 };
