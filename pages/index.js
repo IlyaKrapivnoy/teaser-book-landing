ffe739bc22f7452bf0b057d8ab88fa80cbd899b5
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-06-01 21:37:09 +0300

    split home page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,2 @@
-import Head from 'next/head';
-
-function Home() {
-    return (
-        <div>
-            <Head>
-                <title>Book Teaser Website</title>
-                <meta
-                    name='description'
-                    content='For those who likes to read'
-                />
-            </Head>
-
-            <main>hi</main>
-        </div>
-    );
-}
-
+import Home from '../components/page-components/Home';
 export default Home;
