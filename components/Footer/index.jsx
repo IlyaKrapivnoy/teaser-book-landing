c0862033e6da0e89da2a2852a6cb8b93585cb168
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-06-06 20:43:15 +0300

    add footer top part

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import styles from './Footer.module.scss';
+import Navbar from '../Navbar';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className={styles.footerWrapper}>
+            <div className={'container'}>
+                <div className={styles.footer}>
+                    <div className={styles.top}>
+                        <Navbar
+                            classNameUl={styles.list}
+                            classNameLi={styles.listItem}
+                        />
+                    </div>
+                    <div className={styles.bottom}></div>
+                </div>
+            </div>
         </div>
     );
 };
