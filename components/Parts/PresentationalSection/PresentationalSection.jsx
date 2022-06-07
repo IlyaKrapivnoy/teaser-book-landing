47b80f996e4a79ecf7b583049b3299fa69a027a7
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-06-07 22:17:01 +0300

    add description

--- components/Parts/PresentationalSection/PresentationalSection.jsx
+++ components/Parts/PresentationalSection/PresentationalSection.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const PresentationalSection = () => {
-    return <div>PresentationalSection</div>;
+    return (
+        <>
+            <p>
+                <span>50% discount</span>on early purchase
+            </p>
+            <h1>The Man in the Glass House</h1>
+            <p>A great landing page to sell your eBook!</p>
+            <p>
+                We work with our partners to streamline project plans that don’t
+                just deliver on product perfection, but also delivers on time.
+            </p>
+            <button>Buy now for $14.99</button>
+            <p>* eBook includes iBooks, PDF & ePub versions</p>
+        </>
+    );
 };
 
 export default PresentationalSection;
