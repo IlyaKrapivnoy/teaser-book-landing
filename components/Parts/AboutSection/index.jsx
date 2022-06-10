537fbb07c7d9437a868403ce4dad7ab0e10db0da
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-06-10 18:57:40 +0300

    add kindle image

--- components/Parts/AboutSection/index.jsx
+++ components/Parts/AboutSection/index.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
+import styles from '../PresentationalSection/PresentationalSection.module.scss';
+import KindleImg from '../../../public/kindle.png';
 
 const AboutSection = () => {
-    return <div>About</div>;
+    return (
+        <div className='container'>
+            <div className={styles.wrapper}>
+                <div className={styles.picturePart}>
+                    <Image
+                        src={KindleImg}
+                        alt={'Kindle'}
+                        width={400}
+                        height={400}
+                    />
+                </div>
+                <div className={styles.textPart}></div>
+            </div>
+        </div>
+    );
 };
 
 export default AboutSection;
