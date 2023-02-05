85661745bab921d26270a0041690f34a75c6cd69
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-02-05 15:06:08 +0100

    type navbar

--- components/partials/Navbar/NavList.tsx
+++ components/partials/Navbar/NavList.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
-const NavList = ({ data }) => {
+interface NavItem {
+    id: number;
+    title: string;
+}
+
+interface Props {
+    data: NavItem[];
+}
+
+const NavList: React.FC<Props> = ({ data }) => {
     return (
         <div className={styles.navbar}>
             <ul className={styles.list}>
--- components/partials/Navbar/NavbarItems.tsx
+++ components/partials/Navbar/NavbarItems.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import navbarItems from '../../../data/navItems';
 
-const NavbarItems = ({ classNameUl, classNameLi }) => {
+interface NavbarItemsProps {
+    classNameUl: string;
+    classNameLi: string;
+}
+
+const NavbarItems: React.FC<NavbarItemsProps> = ({
+    classNameUl,
+    classNameLi,
+}) => {
     return (
         <ul className={classNameUl}>
             {navbarItems.map((navItem) => (
